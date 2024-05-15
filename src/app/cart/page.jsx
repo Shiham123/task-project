524a656926d1eefca94a10fe9ef6983502004764
Shiham123 <s36651786@gmail.com>
2024-05-15 10:20:24 +0600

    cart section running

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/Footer"
 import NavbarSection from "@/components/Navbar"
 
 const Cart = () => {
 	return (
 		<div>
 			<NavbarSection />
+			<Footer />
 		</div>
 	)
 }
