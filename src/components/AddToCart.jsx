fd54e2062cebdcae17d2b61b0702e794c43aece3
Shiham123 <s36651786@gmail.com>
2024-05-15 17:51:53 +0600

    successfully get data and based on id filter

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,4 +1,7 @@
+"use client"
+
 import {Poppins} from "next/font/google"
+import {useEffect, useState} from "react"
 
 const poppins = Poppins({
 	subsets: ["latin"],
@@ -7,6 +10,34 @@ const poppins = Poppins({
 })
 
 const AddToCart = () => {
+	const [isLoading, setIsLoading] = useState(true)
+	const [addedCart, setAddedCart] = useState([])
+	const [filterCartItem, setFilterCartItem] = useState([])
+
+	useEffect(() => {
+		fetch("https://fakestoreapi.com/products")
+			.then((res) => res.json())
+			.then((data) => {
+				setIsLoading(false), setAddedCart(data)
+			})
+			.catch((error) => {
+				throw new Error(error)
+			})
+	}, [])
+
+	useEffect(() => {
+		const getAddedCartIds = JSON.parse(localStorage.getItem("cart"))
+
+		if (getAddedCartIds && getAddedCartIds.length > 0) {
+			const filteredCart = addedCart.filter((item) => getAddedCartIds.includes(item.id))
+			setFilterCartItem(filteredCart)
+		}
+	}, [addedCart])
+
+	console.log(filterCartItem)
+
+	if (isLoading) return <p>...loading</p>
+
 	return (
 		<div className={`${poppins.className} lg:col-span-4 md:hidden hidden lg:block mt-[11rem]`}>
 			<div className="border-[1px] border-[#212529] p-2 flex flex-col justify-end items-end rounded-lg">
@@ -23,6 +54,9 @@ const AddToCart = () => {
 						</span>
 					</p>
 				</div>
+
+				{/* here get local storage value */}
+
 				<button className="bg-[#212529] px-4 text-white py-3 rounded-lg text-lg tracking-wide uppercase">
 					Checkout
 				</button>
